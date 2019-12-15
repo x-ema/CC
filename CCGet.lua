c87278ee0e27da69bd2023f6bec1a5d822852640
x-ema <58913581+x-ema@users.noreply.github.com>
2019-12-15 09:33:37 -0800

    Update CCGet.lua

--- CCGet.lua
+++ CCGet.lua
@@ -2,7 +2,7 @@ local func = io.read()
 if func == "get" then
 	local url = io.read()
 	local name = io.read()
-	_ = io.open(name,"w")
-	_.write(http.get("https://raw.githubusercontent.com/x-ema/CC/master/" .. url).readAll())
-	_.close()
+	FILE = fs.open(name,"w")
+	FILE.write(http.get("https://raw.githubusercontent.com/x-ema/CC/master/" .. url).readAll())
+	FILE.close()
 end
