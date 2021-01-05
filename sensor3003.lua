ba861652b88af9df1c5b3001a6888a2b7a08626d
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-05 09:08:43 -0800

    Update sensor3003.lua

--- sensor3003.lua
+++ sensor3003.lua
@@ -8,7 +8,7 @@ function getPlayers()
   local dynmap_url = 'http://tekkit.craftersland.net:25800/up/world/world/'
   local dynmap_data = http.get(dynmap_url).readAll()
   local names = dynmap_data:gmatch('"name":"%a+"')
-  print(names)
+  for i = 1,50 do names() end
 end
 
 getPlayers()
