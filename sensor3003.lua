c849f948ae1321c47a52ba953fed8e6cdb9bf241
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-05 09:49:04 -0800

    Update sensor3003.lua

--- sensor3003.lua
+++ sensor3003.lua
@@ -4,11 +4,25 @@ function getInventory(name)
   print(textutils.serialize(sen.getPlayerData(name).inventory))
 end
 
+--[[add regex's ability to find all matches in a string to lua]]--
+function findAll(str,pattern)
+  local results = {}
+  search_start = 1
+  while true do
+    start,last = str:find(pattern, search_start)
+    if start ~= nil and last ~= nil then
+      results.insert({start,last})
+      search_start = last
+    else return results
+    end
+  end
+end
+
 function getPlayers()
   local dynmap_url = 'http://tekkit.craftersland.net:25800/up/world/world/'
   local dynmap_data = http.get(dynmap_url).readAll()
-  local names = dynmap_data:gmatch('"name":"%a+"')
-  for i = 1,50 do names() end
+  local names = findAll(dynmap_data,'"name":"%a+"')
+  for i = 1,#names do print(names[i][1]) print(names[i][2]) end
 end
 
 getPlayers()
