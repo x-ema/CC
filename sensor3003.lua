94ed2ea9065d199945fc3ec7df14777ef8480629
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-05 08:48:24 -0800

    Update sensor3003.lua

--- sensor3003.lua
+++ sensor3003.lua
@@ -1,7 +1,14 @@
-dynmap_url = 'http://tekkit.craftersland.net:25800/up/world/world/'
+
 sen = peripheral.wrap('back')
-sen.getPlayerData(name)
 
 function getInventory(name)
   print(textutils.serialize(sen.getPlayerData(name).inventory))
+end
+
+function getPlayers()
+  local dynmap_url = 'http://tekkit.craftersland.net:25800/up/world/world/'
+  local dynmap_data = json.decode(http.get(dynmap_url).readAll())
+  for _,x in pairs(dynmap_data) do
+    print(x)
+  end
 end
