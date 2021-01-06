2440ed1b423efcb5cef89ed9c929c1758955b1de
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-06 11:11:48 -0800

    Update invsee.lua

--- invsee.lua
+++ invsee.lua
@@ -14,22 +14,30 @@ function drawInv(inventory)
   liney = 5
   linex = 1
   inv_margin = 20
+  inv_bg = 0x404040
+  inv_fg = 0x9e9e9e
   for i = 9,36 do --[[draw main body of inventory]]--
+    glass.addBox(linex * inv_margin,liney * invmargin,inv_margin,inv_margin,inv_bg,1)
+    glass.addBox(linex * inv_margin,liney * invmargin,inv_margin-2,inv_margin-2,inv_fg,1)
     glass.addIcon(linex * inv_margin,liney * inv_margin,inventory[i].id,inventory[i].dmg)
     if linex == 9 then linex = 1 liney = liney + 1 else linex = linex + 1 end
   end
   liney = 8
   linex = 1
   for i = 1,9 do --[[draw the hotbar at the bottom of gui]]--
+    glass.addBox(linex * inv_margin,liney * invmargin,inv_margin,inv_margin,inv_bg,1)
+    glass.addBox(linex * inv_margin,liney * invmargin,inv_margin-2,inv_margin-2,inv_fg,1)
     glass.addIcon(linex * inv_margin,liney * inv_margin,inventory[i].id,inventory[i].dmg)
     linex = linex + 1
   end
   linex = 1
   liney = 1
   for i = 37,40 do --[[draw armor slots to right of gui]]--
+    glass.addBox(linex * inv_margin,liney * invmargin,inv_margin,inv_margin,inv_bg,1)
+    glass.addBox(linex * inv_margin,liney * invmargin,inv_margin-2,inv_margin-2,inv_fg,1)
     glass.addIcon(linex * inv_margin,liney * inv_margin,inventory[i].id,inventory[i].dmg)
     liney = liney + 1
   end
 end
 
-drawinv(getInv('Sleetyy'))
+drawInv(getInv('Sleetyy'))
