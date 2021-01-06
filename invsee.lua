64780a1349a5e70deb6a6cae9763d42063a3e007
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-06 15:23:55 -0800

    Update invsee.lua

--- invsee.lua
+++ invsee.lua
@@ -1,53 +1,40 @@
 glass = peripheral.wrap('back')
 sen = peripheral.wrap('top')
---glass.addIcon = 16x16
---glass.addIcon(x,y,id,meta)
 
-function getInv(player)
-  inventory = sen.getPlayerData(player).inventory
-  if inventory ~= nil then return inventory else return false end
+function drawItem(x,y,id,dmg)
+  local margin = 20
+  local bg = 0x404040
+  local fg = 0x9e9e9e
+  glass.addBox((x * margin) - 1,(y * margin) - 1,margin,margin,bg,1)
+  glass.addBox((x * margin) - 1,(y * margin) - 1,margin - 2,margin - 2,bg,1)
+  glass.addIcon(x * margin,y * margin,id,dmg)
 end
 
---[[
-inv scructure
-slot 1-9 = hotbar
-last 4 slots = armor (40,39,38,37)?
-]]--
-function drawInv(inventory)
-  if inventory == false then print('Player does not exist/is not online') end
-  liney = 5
-  linex = 1
-  inv_margin = 20
-  inv_bg = 0x404040
-  inv_fg = 0x9e9e9e
-  for i = 10,36 do --[[draw main body of inventory]]--
-    glass.addBox(linex * inv_margin,liney * inv_margin,inv_margin,inv_margin,inv_bg,1)
-    glass.addBox(linex * inv_margin,liney * inv_margin,inv_margin-2,inv_margin-2,inv_fg,1)
-    glass.addIcon(linex * inv_margin,liney * inv_margin,inventory[i].id,inventory[i].dmg)
-    if linex == 9 then linex = 1 liney = liney + 1 else linex = linex + 1 end
+function invsee(sen,player)
+  local inventory = sen.getPlayerData(player).inventory
+  if not inventory then error('Player does not exist/is not online') end
+  row = 5
+  column = 1
+  for i = 10,36 do --[[Draw main inv]]--
+    drawItem()
+    if row == 9 then row = 1 column = column + 1 else row = row + 1 end
   end
-  liney = 8
-  linex = 1
-  for i = 1,9 do --[[draw the hotbar at the bottom of gui]]--
-    glass.addBox(linex * inv_margin,liney * inv_margin,inv_margin,inv_margin,inv_bg,1)
-    glass.addBox(linex * inv_margin,liney * inv_margin,inv_margin-2,inv_margin-2,inv_fg,1)
-    glass.addIcon(linex * inv_margin,liney * inv_margin,inventory[i].id,inventory[i].dmg)
-    linex = linex + 1
+  row = 8
+  column = 1
+  for i = 1,9 do --[[Draw hot bar]]--
+    drawItem()
+    row = row + 1
   end
-  linex = 1
-  liney = 1
-  for i = 37,40 do --[[draw armor slots to right of gui]]--
-    glass.addBox(linex * inv_margin,liney * inv_margin,inv_margin,inv_margin,inv_bg,1)
-    glass.addBox(linex * inv_margin,liney * inv_margin,inv_margin-2,inv_margin-2,inv_fg,1)
-    glass.addIcon(linex * inv_margin,liney * inv_margin,inventory[i].id,inventory[i].dmg)
-    liney = liney + 1
+  row = 1
+  column = 1
+  for i = 37,40 do --[[Draw armor slots]]--
+    drawItem()
+    column = column + 1
   end
 end
-
-
-
+  
 while true do
   _,msg,user = os.pullEvent('chat_command')
-  if msg:find('invsee') then drawInv(getInv(msg:gsub('invsee ',''))) end
+  if msg:find('invsee') then sen:invsee(msg:gsub('invsee ','')) end
   if msg == 'clear' then glass.clear() end
 end
