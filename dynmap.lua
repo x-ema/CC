82a3498e9347371b7f943525ffd12b5efb4cfd40
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-06 17:45:57 -0800

    Update dynmap.lua

--- dynmap.lua
+++ dynmap.lua
@@ -1,3 +1,11 @@
 --[[Functions]]--
-
+function mount(peripheral_name)
+ if peripheral_name == 'help' then print('usage "peripheral_name":mount(). ex pname = peripheral.wrap("monitor":mount())') end
+ for _,perip in pairs(peripheral.getNames()) do
+   if peripheral.getType(perip) == peripheral_name then
+    return perip
+   end
+ end
+end
 --[[Main]]--
+sensor = '
