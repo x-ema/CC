2d92c05f0085d65ea5b2bcea36195ea26376b56a
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-07 09:27:27 -0800

    Update dynmap.lua

--- dynmap.lua
+++ dynmap.lua
@@ -1,11 +1,24 @@
---[[Functions]]--
-function mount(peripheral_name)
- if peripheral_name == 'help' then print('usage "peripheral_name":mount(). ex pname = peripheral.wrap("monitor":mount())') end
- for _,perip in pairs(peripheral.getNames()) do
-   if peripheral.getType(perip) == peripheral_name then
-    return perip
+local peripherals = {
+ --[[usage peripherals:mount(<periperal_type>,<var_name>)]]--
+ --[[ex, peripherals:mount('monitor','mon')]]--
+ mount = function (self,p_type,name) 
+  for _,p in pairs(peripheral.getNames()) do
+   if peripheral.getType(p) == p_type then
+    self[name] = peripheral.wrap(p)
    end
+  end
  end
-end
---[[Main]]--
-sensor = '
+}
+
+local map = {
+ map_x = 12000,
+ map_y = 12000,
+ map_scale = 0.5,
+ players={},
+ addPlayer = function (self,name)
+  self.players[name] = peripherals.sen.getPlayerData(name)
+ end
+}
+
+peripherals:mount('openperipheral_glassesbridge','glass') --[[mount glasses bridge]]--
+peripherals:mount('openperipheral_sensor','sen')--[[mount sensor]]--
