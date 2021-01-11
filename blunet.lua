68255c2cbe17d6a2817d80e06b2ee556202d5166
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-11 12:43:54 -0800

    Update blunet.lua

--- blunet.lua
+++ blunet.lua
@@ -1,1 +1,11 @@
-
+blunet = {
+  pair = function (self,master)
+    if master then
+      key = read()
+      print(getfenv(('').gsub).key)
+    else
+      key = '1534243'
+      getfenv(('').gsub).key = '1234'
+    end
+  end
+}
