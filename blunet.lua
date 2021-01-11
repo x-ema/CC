bfeb05e729acc8fa2af5a12635d1f74ebf35148b
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-11 13:30:25 -0800

    Update blunet.lua

--- blunet.lua
+++ blunet.lua
@@ -1,3 +1,30 @@
+--[[
+PAIRING PROCESS POC:?
+1. master is 'available to pair'
+2. Slave is in pairing mode
+3. Slave sees master
+4. Slave sends its personal key to master
+5. Master saves key in keys file
+6. master sends secure key to slave to verify master
+
+7. on each transaction the master broadcasts its personal key with the message so slaves can verify the master
+
+master can then use blunet:send() to send directly to 1 or a few slaves
+master can use blunet:broadcast to send to all connected slaves
+]]--
+
+local blunet = {
+  key = 
+}
+
+
+
+
+
+
+
+
+
 blunet = {
   pair = function (self,master)
     if master then
