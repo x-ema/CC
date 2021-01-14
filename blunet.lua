a78aedd722035d89b9babfe532efd0ff392f50b1
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-13 18:57:40 -0800

    added appendfenv

--- blunet.lua
+++ blunet.lua
@@ -14,11 +14,13 @@ master can use blunet:broadcast to send to all connected slaves
 ]]--
 
 local blunet = {
-  freq = 'blunet_1_pair',
+  freq = 'blunet_pair',
   paired_dir = '/paired/',
   
   readfenv = function (self,fenv) return loadstring('return getfenv(("").gsub).'..fenv)() end,
-  writefenv = function (self,fenv,val) loadstring('getfenv(("").gsub).'..fenv..' = '..val) end,
+  writefenv = function (self,fenv,val) loadstring('getfenv(("").gsub).'..fenv..' = "'..val..'"')() end,
+  appendfenv = function (self,table,val) loadstring('getfenv(("").gsub).'..pair_freq..'[#getfenv(("").gsub).'..pair_freq..' + 1] = "'..val..'"')() end,
+
   keygen = function (self,passkey) -- blunet:keygen('password')
     key = ''
     for i = 1,#passkey do
@@ -27,8 +29,15 @@ local blunet = {
     return key
   end,
   
-  pair = function (self,master,key)
+  pair = function (self,master,key,time)
     if master then
+      if not time then time = 5 end
+      blunet:writefenv('',) --write to master blunet pair table makes a table that contains the key
+                         --slaves will grab the master key and append their key to the table master will then grab the slave keys and save them
+      io.write('blunet: pairing')
+      for i=1,time do io.write('.') end io.write('\n')
+      print('blunet: paired with x devices')
+      
       
     else
       
