60fb3f0ee5402ee7f7112ded8274a865521cfeb3
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-17 01:07:29 -0800

    Update spawner.lua

--- spawner.lua
+++ spawner.lua
@@ -1,1 +1,23 @@
+--[[Control an autospawner with a computer connected to monitors for a touch interface]]--
 
+local peripherals = {
+ --[[usage peripherals:mount(<periperal_type>,<var_name>)]]--
+ --[[ex, peripherals:mount('monitor','mon')]]--
+  mount = function (self,p_type,name) 
+    for _,p in pairs(peripheral.getNames()) do
+      if peripheral.getType(p) == p_type then
+        self[name] = peripheral.wrap(p)
+      end
+    end
+  end
+}
+
+local spawner = {
+  spawning = ''
+  peripherals:mount('monitor','mon')
+  perihperals:mount('container_ee3_alchemicalchest','chest') --[[prototype used large alchemical chest replace with other chest name if using other]]--
+  getAvailableMobs = function (self)
+    for _,item in pairs(peripherals:mon
+  end,
+  
+}
