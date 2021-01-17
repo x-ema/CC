94a0f8c103bf82dd727692d99f3861b8c954f37d
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-17 01:36:48 -0800

    Update spawner.lua

--- spawner.lua
+++ spawner.lua
@@ -14,10 +14,29 @@ local peripherals = {
 
 local spawner = {
   spawning = ''
-  peripherals:mount('monitor','mon')
-  perihperals:mount('container_ee3_alchemicalchest','chest') --[[prototype used large alchemical chest replace with other chest name if using other]]--
+  available = {}
+  peripherals:mount('monitor','mon'),
+  perihperals:mount('container_ee3_alchemicalchest','chest'), --[[prototype used large alchemical chest replace with other chest name if using other]]--
+  condense = function (self) peripherals.chest.condenseItems() end,
   getAvailableMobs = function (self)
-    for _,item in pairs(peripherals:mon
+    self.available = {}
+    self.condense()
+    for _,item in pairs(peripherals.chest.getAllStacks()) do
+      self.available[#self.available + 1] = item.captured
+    end
   end,
-  
+  isAvailable = function (self,mob_name)
+    for _,mob in pairs(self.available) do
+      if mob == mob_name do return end
+    end
+    return false
+  end,
+  switchNet = function (self,tospawn)
+    avail = self.isAvailable(tospawn)
+    if avail then
+      peripherals.chest.pullItem('up',1)
+      peripherals.chest.pushItem('up',avail)
+    end
+  end,
+  removeNet = function (self) peripherals.chest.pullItem('up',1) end
 }
