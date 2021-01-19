3546c48161236912a187f5673ee621fc5e3daf87
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-19 11:07:47 -0800

    Update ender_chest_scan.lua

--- ender_chest_scan.lua
+++ ender_chest_scan.lua
@@ -1,15 +1,12 @@
-ochest = peripheral.wrap("top")
-
-
-while true do
-	for i=1,4096 do
-		oChest.setFrequency(i)
-		if #oChest ~= 0 then
-			print("Located " .. i)
-			oChest.condenseItems()
-			for ii=1,#oChest.getAllStacks() do
-				oChest.pushItem("south",ii)
-			end
+chest = peripheral.wrap('top')
+for i = 0,4095 do
+	chest.setFrequency(i)
+	all_items = chest.getAllStacks()
+	if #all_items > 0 then
+		print('Freq : '..i)
+		for x = 1,#all_items do
+			chest.pushItem('up',x)
+			print('		found '..all_items[x].name..' X'..all_items[x].qty)
 		end
 	end
 end
