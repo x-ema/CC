23ea0d9585398a855fe05fdd5df5c6d0e228db78
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-19 12:59:25 -0800

    Update obsidian_gen.lua

--- obsidian_gen.lua
+++ obsidian_gen.lua
@@ -1,2 +1,21 @@
-input = peripheral.wrap('left')
-output = peripheral.wrap('right')
+empty = peripheral.wrap('left')
+full = peripheral.wrap('right')
+
+north = 'north'
+south = 'south'
+
+while true do
+  repeat
+    sleep(0.05)
+  until #full.getAllStacks() > 0
+  full.pushItem(south,1)
+  turtle.place()
+  repeat
+    sleep(0.05)
+  until turtle.detect()
+  turtle.select(2)
+  turtle.dig()
+  turtle.dropDown()
+  turtle.select(1)
+  empty.pullItem(north,1)
+end
