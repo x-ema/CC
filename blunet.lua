e58eb9fd47078ef2283be1c67a4a3156cca96383
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-23 23:53:04 -0800

    concept code completed
    time for bug testing in game

--- blunet.lua
+++ blunet.lua
@@ -13,62 +13,75 @@ master can then use blunet:send() to send directly to 1 or a few slaves
 master can use blunet:broadcast to send to all connected slaves
 ]]--
 
-local blunet = {
-  pair_freq = getfenv(('').gsub)['pair_freq'],
+os.loadAPI('sha256') --requires sha256 lib by MaHuJa
+
+
+local blunet = { --only supports one way connections atm (master to slaves)
+  pair_freq = nil
   dir = 'paired',
+  key = ''
+  identifier = 0
   
   setPairFreq = function (self,pair_freq) self.pair_freq = getfenv(('').gsub)[pair_freq] end,
+  getPaired = function (self) return fs.list(dir) end,
+  returnKey = function (self,key_str) return sha256.sha256(key_str) end,
+  keygen = function (self,key_str) self.key = sha256.sha256(key_str) end,
+  append = function (self,table,val) table[#table + 1] = val end,
+  saveKey = function (self,key) if fs.exists(self.dir + key) then return true else f = fs.open(self.dir..'/'..key,'w') f.write(key) f.close() return true end end,
+  readKeys = function (self) if #fs.list(self.dir) > 0 then return fs.list(self.dir)[1] else error('No devices paired') end end,
   
- 
-}
-
-
-local blunet_old = {
-  pair_freq = 'blunet_pair',
-  paired_dir = '/paired/',
   
-  readfenv = function (self,fenv) return loadstring('return getfenv(("").gsub).'..fenv)() end,
-  writefenv = function (self,fenv,val) loadstring('getfenv(("").gsub).'..fenv..' = "'..val..'"')() end,
-  appendfenv = function (self,table,val) loadstring('getfenv(("").gsub).'..table..'[#getfenv(("").gsub).'..table..' + 1] = "'..val..'"')() end,
-  
-  setpairfreq = function (self,freq) pair_freq = freq end, --use this to creare unique pair freq to avoid confusing pcs when pairing
-  mkdir = function (self,dir) shell.run('mkdir '..dir) end,
-  
-
-  
-  pair = function (self,master,key,time)
+  pair = function (self,master,key,timeout)
+    if not pair_freq then error('You must specify a pair_Freq before pairing, use blunet:setPairFreq("<freq>") to pair.') end
+    if not key then error('You must specify a key for this PC before pairing, use blunet:keygen("<key>") to set a key') end
     if master then
-      if not time then time = 5 end
-      blunet:writefenv(pair_freq,'{'..key..'}')
-      --master writes to the blunet pair table (slight limit, can only have 1 pairing process at one time on the server, can be remidied by using unique pair_freq)
-      --slave grabs master key and append their own to the pair table.
-      --master grabs slave keys and saves them in /paired/
-      --slaves will save master key in /paired/master.key
-      io.write('blunet: pairing')
-      for i=1,time do io.write('.') end io.write('\n')
-      paired = blunet:readfenv(pair_freq)
-      print('blunet: paired with '..tostring(#paired - 1)..' devices')
-      for i = 1,#paired - 1 do
-        --save paired into file
-      end
+      pair_freq.key = {key} --solved 1 pairer at a time by using a user defined pair_freq which will hopefully never be the same
+      term.clear()
+      term.setCursorPos(1,1)
+      print('Press any key to stop pairing...')
+      os.pullEvent('key')
+      for i = 2,#pair_freq do self:saveKey(pair_freq[i]) end
+      print('Paired with '..tostring(#paire_freq - 1)..' devices')
     else
-      pairing = blunet:readfenv(pair_freq)
-      if pairing then
-        --save the first value of pairing as master key
-        blunet:appendfenv(pair_freq,key)
-        print('blunet: paired with master')
-      else
-        print('blunet: no blunet masters detected on freq "'..pair_freq..'"')
-      end
+      if self.pair_freq = {} then error('No masters pairing on this freq') end
+      saveKey(self.pair_freq[1])
+      self:append(self.pair_Freq,self.key)
     end
   end,
-  broacast = function (self,key,msg)
-    --write to all saved slaves tables with msg and key
+  broadcast = function (self,data)
+    if not data then error('Usage blunet:broadcast(<data>') end
+    if not self.key then error('You must specify a key for this PC before pairing, use blunet:keygen("<key>") to set a key') end
+    math.randomseed(os.time())
+    getfenv(('').gsub)[self.key] = {key = self.key, data = data, id = math.random(1,1000000)}
+    --sends data to the 'global' channel and sends a unique identifier to allow for multiple of the same message
   end,
-  send = function (self,key,slave_key,msg)
-    --write to specific saved slaves tables with msg and key
+  
+  send = function (self,key,data)
+    if not key or data then error('Usage blunet:send(<key>,<data>)') end
+    math.randomseed(os.time())
+    getfenv(('').gsub)[key] = {key = key, data = data, id = math.random(1,1000000)}
+    --sends data to a specific key or slave, also a unique id  to allow for multile of the same message
+  end,
+  
+  receive = function (self) --issue: accidently running this on a master node could cause problems since it will have to sift through however many paired slaves it has. will either just receive data incorrectly or crash not sure yet
+    key = self:readKeys()
+    global = getfenv(('').gsub)[key]
+    loc = getfenv(('').gsub)[self.key]
+    repeat
+      if global then
+        if global.key == key and self.identifier ~= global.id then
+          self.identifier = global.id
+          return global.data
+        end
+      elseif loc then
+        if loc.key = self.key and self.identifier ~= global.id then
+          self.identifier = global.id
+          return loc.data
+        end
+      end
+      --check device specific channel and the 'global' channel and compare the unique identifier to make sure it isnt reading a duplicate message
+    until data ~= nil
   end
-}
+ }
 
-
-blunet:keygen('password')
+--concept code not yet bugtested
