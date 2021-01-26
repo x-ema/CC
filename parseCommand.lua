e993b9569b8d32eb27427fcc350aacedb4803c38
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-26 07:48:35 -0800

    Delete parseCommand.lua

--- parseCommand.lua
+++
@@ -1,9 +0,0 @@
-_,msg,user = os.pullEvent('chat_command')
-
-function split(str)
-  words = {}
-  for word in str:gmatch('%S+') do words[#words+1] = word end
-  return words
-end
-
-cmd = msg:split()
