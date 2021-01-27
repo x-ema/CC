e8ed1469181ede6b3440b519baeb52b1603c820f
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-27 11:27:02 -0800

    Update eat_chunk.lua

--- eat_chunk.lua
+++ eat_chunk.lua
@@ -4,6 +4,7 @@ local tArgs={...}
 local turt = {
   dig = function (self) turtle.digUp(); turtle.dig(); turtle.digDown(); end,
   forward = function (self,num) if not num then num = 1 end; for i = 1,num do turtle.forward() end end,
+  isEven = function (self,num) if num/2 == math.ceil(num/2) then return true else return false end end,
   
   quarry = function (self,size_x,size_z)
     for x = 1,tonumber(size_x) do
@@ -11,9 +12,15 @@ local turt = {
         self:dig()
         turtle.forward()
       end
-      turtle.turnLeft()
-      turtle.forward()
-      turtle.turnLeft()
+      if self:isEven(x) then
+        turtle.turnLeft()
+        turtle.forward()
+        turtle.turnLeft()
+      else
+        turtle.turnRight()
+        turtle.forward()
+        turtle.turnRight()
+      end
     end
   end,
 }
