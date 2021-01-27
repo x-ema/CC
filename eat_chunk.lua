0c6b733988a8836f926f8f41a9f4310c1492e06e
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-27 11:01:26 -0800

    Update eat_chunk.lua

--- eat_chunk.lua
+++ eat_chunk.lua
@@ -1,5 +1,8 @@
 --early game turtle quarry concept
 
 local turt = {
-
+  dig = function (self) turtle.digUp() turtle.dig() turtle.digDown() end,
+  forward = function (self,num) for i = 1,num do turtle.forward() end end,
+  
+  
 }
