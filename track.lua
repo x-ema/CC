2084f3de4ec0573c7d58385675b5aad80ce794a9
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-27 15:29:01 -0800

    Update track.lua

--- track.lua
+++ track.lua
@@ -1,35 +1,19 @@
-local track = {
-  sen_pos = {x=4887,y=6,z=3572},
-  glass = peripheral.wrap('top'),
-  sen = peripheral.wrap('bottom'),
-  tracking = 'Sleetyy',
-  
-  doTrack = function (self)
-    sen_data = peripheral.call('bottom','getPlayerData',self.tracking)
-    print(sen_data.username)
-    print(sen_data.position.x)
-    print(sen_data.position.y)
-    print(sen_data.position.z)
-    
-      if data then
-        glass.clear()
-        glass.addBox(10,10,100,20,0xFFFFFF,0.5)
-        glass.addText(15,15,data.username..': x'..data.position.x..' y'..data.position.y..' z'..data.position.z,0xFF1100)
-      end
-  end,
-  
-  track = function (self)
-    parallel.waitForAny(
-      function()
-        _,msg,usr = os.pullEvent('chat_command')
-        if usr == 'Sleetyy' and #msg > 1 then self.tracking = msg end
-      end,
-      pcall(function()
-        sleep(2)
-        self:doTrack()
-      end)
-    )
-  end
-}
+local sen_pos = {x=4887,y=6,z=3572}
+local glass = peripheral.wrap('top')
+local tracking = 'Sleetyy'
 
-while true do track:track() end
+parallel.waitForAny(
+  function()
+    _,msg,usr = os.pullEvent('chat_command')
+    if usr == 'Sleetyy' then tracking = usr end
+    print(usr..' : '..msg)
+  end,
+  function()
+    sleep(2)
+    data = peripheral.call('bottom','getPlayerData',tracking)
+    print(data.position.x)
+    print(data.position.y)
+    print(data.position.z)
+    print(data.username)
+  end
+)
