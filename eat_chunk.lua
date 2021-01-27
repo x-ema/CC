96e5bac97c3e7748aa915f61ca21be8b9def0cd1
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-27 11:55:06 -0800

    Update eat_chunk.lua

--- eat_chunk.lua
+++ eat_chunk.lua
@@ -2,34 +2,26 @@
 local tArgs={...}
 
 local turt = {
+  chest = 1
   dig = function (self) turtle.digUp(); turtle.dig(); turtle.digDown(); end,
   forward = function (self,num) if not num then num = 1 end; for i = 1,num do turtle.forward() end end,
   isEven = function (self,num) if num/2 == math.ceil(num/2) then return true else return false end end,
   
-  quarry = function (self,size_x,size_z,y_size)
-    for y = 1,tonumber(size_y) do
-      for x = 1,tonumber(size_x) do
-        for z = 1,tonumber(size_z)-1 do
-          self:dig()
-          turtle.forward()
-        end
-        if self:isEven(x) then
-          turtle.turnRight()
-          turtle.forward()
-          self:dig()
-          turtle.turnRight()
-        else
-          turtle.turnLeft()
-          turtle.forward()
-          self:dig()
-          turtle.turnLeft()
-        end
-      end
-      turtle.down()
-      turtle.down()
-      turtle.down()
+  wellMine = function (self)
+    dwn = 0
+    while true do
+      turtle.dig()
+      turtle.digDown()
+      if not turtle.down() then break else dwn = dwn + 1 end
     end
+    for i = 1,dwn do turtle.up() end
+    turtle.select(1)
+    for i = 2,16 do turtle.dropUp() end
   end,
 }
 
-turt:quarry(16,16)
+for i = 1,8 do
+  turt:wellMine()
+  turtle.forward()
+  turtle.forward()
+end
