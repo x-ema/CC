aa68fe1728d83c0ba020fc348b39e913c727fcb0
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-27 15:34:02 -0800

    Update track.lua

--- track.lua
+++ track.lua
@@ -11,9 +11,18 @@ parallel.waitForAny(
   function()
     sleep(2)
     data = peripheral.call('bottom','getPlayerData',tracking)
-    print(data.position.x)
-    print(data.position.y)
-    print(data.position.z)
-    print(data.username)
+    if data then
+      glass.clear()
+      glass.addBox(10,10,100,10,0xFFFFFF,0.5)
+      glass.addText(15,15,data.username..': x'..data.position.x+sen_pos.x..' y'..data.position.y+sen_pos.y..' z'..data.position.z+sen_pos.z,0xFF1100)
+      print(data.position.x)
+      print(data.position.y)
+      print(data.position.z)
+      print(data.username)
+    else
+      glass.clear()
+      glass.addBox(10,10,100,10,0xFFFFFF,0.5)
+      glass.addText(15,15,'Player not available...',0xFF1100)
+    end
   end
 )
