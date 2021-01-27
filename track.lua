888445aa1ff42b6a66ac68c7d6c4a145af77c980
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-27 15:37:06 -0800

    Update track.lua

--- track.lua
+++ track.lua
@@ -1,11 +1,11 @@
 local sen_pos = {x=4887,y=6,z=3572}
-local glass = peripheral.wrap('top')
+local glass = peripheral.call('top','getUserSurface','Sleetyy')
 local tracking = 'Sleetyy'
 
 parallel.waitForAny(
   function()
     _,msg,usr = os.pullEvent('chat_command')
-    if usr == 'Sleetyy' then tracking = usr end
+    if usr == 'Sleetyy' then tracking = msg end
     print(usr..' : '..msg)
   end,
   function()
@@ -14,14 +14,14 @@ parallel.waitForAny(
     if data then
       glass.clear()
       glass.addBox(10,10,100,10,0xFFFFFF,0.5)
-      glass.addText(15,15,data.username..': x'..data.position.x+sen_pos.x..' y'..data.position.y+sen_pos.y..' z'..data.position.z+sen_pos.z,0xFF1100)
+      glass.addText(15,15,data.username..': x '..math.ceil(data.position.x+sen_pos.x)..' y '..math.ceil(data.position.y+sen_pos.y)..' z '..math.ceil(data.position.z+sen_pos.z),0xFF1100)
       print(data.position.x)
       print(data.position.y)
       print(data.position.z)
       print(data.username)
     else
       glass.clear()
-      glass.addBox(10,10,100,10,0xFFFFFF,0.5)
+      glass.addBox(10,10,1000,25,0xFFFFFF,0.5)
       glass.addText(15,15,'Player not available...',0xFF1100)
     end
   end
