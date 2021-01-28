8514a3eb7178ad101d26d657947bb6c8c92887a6
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-28 11:32:00 -0800

    Update craft.lua

--- craft.lua
+++ craft.lua
@@ -3,10 +3,23 @@ os.loadAPI('rec')
 
 ae = peripheral.wrap('bottom')
 
-craft = function(id,dmg,qty)
-
+craft = function(name,qty)
+  turtle.select(1)
+  if rec[name] then
+    for i = #rec[name],1,-1 do
+      ae.extractItem({id = rec[name][1],dmg = rec[name][2],qty = qty})
+      turtle.transferTo(i)
+    end
+    turtle.select(4)
+    turtle.craft()
+    ae.extractItem(4,turtle.getItemCount(4),'up')
+    end
+  end
 end
+
+--[[
 ae.extractItem({id=1,dmg=0,qty=0},'up')
 ae.insertItem(slot,qty,'up')
 ae.containsItemType(id,dmg)
 ae.countOfItemType(id,dmg)
+]]--
