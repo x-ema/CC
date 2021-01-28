676be506baaf9a894d3492f75d74fcd6315315b5
x-ema <58913581+x-ema@users.noreply.github.com>
2021-01-28 11:59:13 -0800

    Update craft.lua

--- craft.lua
+++ craft.lua
@@ -1,20 +1,23 @@
 --[[AE autocrafting through turtles]]--
 os.loadAPI('rec')
 
-ae = peripheral.wrap('bottom')
+local ae = peripheral.wrap('bottom')
 
-craft = function(name,qty)
+
+local craft = function(name,qty) --working
+  local tslots = {1,2,3,5,6,7,9,10,11}
   turtle.select(1)
   if rec[name] then
     for i = #rec[name],1,-1 do
       ae.extractItem({id = rec[name][i][1],dmg = rec[name][i][2],qty = qty},'up')
-      turtle.transferTo(i)
+      turtle.transferTo(tslots[i])
     end
     turtle.select(4)
     turtle.craft()
     ae.extractItem(4,turtle.getItemCount(4),'up')
   end
 end
+
 
 --[[
 ae.extractItem({id=1,dmg=0,qty=0},'up')
