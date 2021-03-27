0360a9801b36d90e15c12239ec2fa2d2710daf06
x-ema <58913581+x-ema@users.noreply.github.com>
2021-03-26 20:47:39 -0700

    Update blood_magic_auto.lua

--- blood_magic_auto.lua
+++ blood_magic_auto.lua
@@ -1,1 +1,15 @@
-
+in_side = 'WEST'
+out_side = 'EAST'
+altar = peripheral.wrap('top')
+while true do
+  item = altar.getStackInSlot(1)
+  if item then
+    waiting = true
+    while waiting do
+      if item ~= altar.getStackInSlot(1) then waiting = false end
+    end
+    altar.pustItem(out_side,1)
+  else
+    altar.pullItem(in_side,1,1)
+  end
+end
