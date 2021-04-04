3eba3586b58ae43ca28092ff2fb9f95aec31b90f
x-ema <58913581+x-ema@users.noreply.github.com>
2021-04-03 23:31:24 -0700

    Update casting.lua

--- casting.lua
+++ casting.lua
@@ -1,1 +1,10 @@
+yeild = function () os.queueEvent("fakeEvent"); os.pullEvent(); end
 
+while true do
+  for _,location in pairs(periperal.getNames())
+    if location:find('peripheral') then
+      peripheral.wrap(location).pushItem('down',2)
+    end
+  end
+  yeild()
+end
