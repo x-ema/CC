a269cfdc3d921e1ef566e3fb85c922ca6ab5516f
x-ema <58913581+x-ema@users.noreply.github.com>
2021-04-03 23:04:25 -0700

    Update spectrolus_auto.lua

--- spectrolus_auto.lua
+++ spectrolus_auto.lua
@@ -1,1 +1,17 @@
+detector = 'front'
+placer = 'bottom' 
+breaker = 'back'
+chest = peripheral.find('chest')
+direction = 'EAST'
 
+local pulse = function(side)
+  rs.setOutput(side,true)
+  sleep(0.1)
+  rs.setOutput(side,false)
+end
+
+local move_color = function()
+
+pulse(placer)
+
+os.pullEvent('redstone')
