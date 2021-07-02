62cb8ddb8089f1b22b78e554de2b6fd4b7af3c81
x-ema <58913581+x-ema@users.noreply.github.com>
2021-07-01 22:48:48 -0700

    Update track.lua

--- track.lua
+++ track.lua
@@ -1,6 +1,6 @@
 os.loadAPI('sha256')
 
-local sen_pos = {x=4887,y=6,z=3572}
+local sen_pos = {x=3850,y=64,z=1120}
 local tracking = ''
 local user = '6acd7cbdda8356a0d0f6df82f0f52308786c6fbcfa1b14499e563bb34637fb79'
 local omit_cl_staff = {'brunyman','DragonSlayer','iim_wolf','eytixis','mikewerf'}
